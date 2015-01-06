b5ac02168289c4dca152c0d61c5ccba3f5d9a615
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2015-01-06 11:07:43 -0800

    Lecture 1 Example 1

---
+++ index.php/index.php
@@ -0,0 +1,17 @@
+<<?php
+class cat{
+	//class color
+	//class size
+	//class body
+}
+class fish{
+	//class color
+	//class size
+	//class body
+}
+class tiger{
+	//class color
+	//class size
+	//class body
+}
+?>
