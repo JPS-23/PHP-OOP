7fa49cb6cc98fb134c2d098182864f9c85c827fc
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2015-01-07 11:09:47 -0800

    Lecture 2: Example 1

--- index.php/index.php
+++ index.php/index.php
@@ -1,4 +1,5 @@
-<<?php
+<?php
+	//EXAMPLE:1
 class cat{
 	//class color
 	//class size
@@ -9,11 +10,12 @@ class fish{
 	//class size
 	//class body
 }
-class {
+class tiger{
 	//class color
 	//class size
 	//class body
 }
+	//EXAMPLE:2
 $cat1 = newCat();
 $cat2 = newCat();
 
@@ -22,7 +24,7 @@ $fish2 = newFish();
 
 $tiger1 = newTiger();
 $tiger2 = newTiger();
-
+	//EXAMPLE:3
 class cat{
 	public$name= "tom";
 	public$breed= "cool breed";
@@ -41,23 +43,75 @@ class tiger{
 	public$gender= "male";
 	public$price=10;
 }
-
+	//EXAMPLE:4
 class cat{
 	public $firstName = "tom";
 	public $lastName = "smith";
 	public $gender = "male";
-	public $price = "25";
+	public $price = 25;
 }
 class fish{
 	public $firstName = "bob";
 	public $lastName = "tomas";
 	public $gender = "male";
-	public $price = "50";
+	public $price = 50;
 }
 class tiger{
 	public $firstName = "will";
 	public $lastName = "wildcat";
 	public $gender = "male";
-	public $price = "100";
+	public $price = 100;
+}
+	//EXAMPLE:5
+	public function myMethod( $argument, $another) {
+}
+?>
+-------------------------------------------------------------------------------------------------------------------------------
+<?php
+	//EXAMPLE:1
+class Cat {
+public $firstName;
+public $lastName;
+public $breed;
+	function __construct($title, $firstName, $lastName, $breed) {
+	$this­>firstName = $firstName;
+	$this­>lastName = $lastName;
+	$this­>breed = $breed;
+	}
+
+	function getName() {
+	return “{$this­>firstName}” .
+	“{$this­>lastName}”;
+	}
+}
+class Fish {
+public $firstName;
+public $lastName;
+public $breed;
+	function __construct($title, $firstName, $lastName, $breed) {
+	$this­>firstName = $firstName;
+	$this­>lastName = $lastName;
+	$this­>breed = $breed;
+	}
+
+	function getName() {
+	return “{$this­>firstName}” .
+	“{$this­>lastName}”;
+	}
+}
+class Tiger {
+public $firstName;
+public $lastName;
+public $breed;
+	function __construct($title, $firstName, $lastName, $breed) {
+	$this­>firstName = $firstName;
+	$this­>lastName = $lastName;
+	$this­>breed = $breed;
+	}
+
+	function getName() {
+	return “{$this­>firstName}” .
+	“{$this­>lastName}”;
+	}
 }
 ?>
