5ef0480d998d81127875150f8c2a847c4bfce9f8
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2015-01-07 10:40:12 -0800

    lecture : example

--- index.php/index.php
+++ index.php/index.php
@@ -9,7 +9,7 @@ class fish{
 	//class size
 	//class body
 }
-class tiger{
+class {
 	//class color
 	//class size
 	//class body
@@ -22,4 +22,23 @@ $fish2 = newFish();
 
 $tiger1 = newTiger();
 $tiger2 = newTiger();
+
+class cat{
+	public$name= "tom";
+	public$breed= "cool breed";
+	public$gender= "male";
+	public$price=10;
+}
+class fish{
+	public$name= "bob";
+	public$breed= "small breed";
+	public$gender= "male";
+	public$price=10;
+}
+class tiger{
+	public$name= "will";
+	public$breed= "awesome breed";
+	public$gender= "male";
+	public$price=10;
+}
 ?>
