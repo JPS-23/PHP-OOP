6876b46f9ffdf7d846acd56d53cf6dd9d147b7fe
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2015-01-14 11:21:57 -0800

    Lecture 5: Example 1

--- index.php/Inheritance.php
+++ index.php/Inheritance.php
@@ -1,6 +1,6 @@
 <?php
-//lecture 4: Example 1
-class Animal{
+//lecture 4: Example 1 
+/* class Animal{
 	public $firstName;
 	public $lastName;
 	public $scientificName;
@@ -35,5 +35,52 @@ class Dog extends Animal {
 }
 
 $cat = new Cat("Cat", "Mellow", "Yellow", "male", 15);
+print "Animal 1 is a" .$cat->getName(); */
+?>
+_______________________________________________________________________________________________________________________________
+<?php //lecture 5: Example 1
+class Animal {
+	public $firstName;
+	public $lastName;
+	public $scientificName;
+	public $gender;
+	public $weight;
+
+	function __construct($scientificName, $firstName, $lastName, $gender, $weight) {
+		$this->scientificName = $scientificName;
+		$this->firstName = $firstName;
+		$this->lastName = $lastName;
+		$this->gender = $gender;
+		$this->weight = $weight;
+	}
+
+	function getName() {
+		return "This is my" .$this->$firstName .
+				"and last" .$this->$lastName .
+				"and this is my scientific-name" .$this->scientificName .".";
+		}
+}
+/**
+* 
+*/
+class Cat extends Animal{
+	
+	function __construct($scientificName, $firstName, $lastName, $gender, $weight, $meow){
+			parent::__construct($scientificName, $firstName, $lastName, $gender, $weight);
+	$this->meow = $meow;
+	}
+	function greet() {
+	return $this->meow;
+	}
+}
+
+class Dog extends Animal {
+	function __construct($scientificName, $firstName, $lastName, $gender, $weight, $bark) {
+			parent::__construct($scientificName, $firstName, $lastName, $gender, $weight);
+	$this->bark = $bark;
+	}
+}
+
+$cat = new Cat("Cat", "Mellow", "Yellow", "male", 15, true);
 print "Animal 1 is a" .$cat->getName();
 ?>
